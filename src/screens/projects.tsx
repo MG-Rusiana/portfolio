4730c485fa287f6837841937521e575027954268
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-05-02 13:45:07 +0800

    some changes on projects screen

--- src/screens/projects.tsx
+++ src/screens/projects.tsx
@@ -11,12 +11,12 @@ import { useState } from 'react';
 export const Projects = () => {
 
     const projects = [
-      {name: "Facebook UI Copy", img: fb},
-      {name: "Youtube UI Copy", img: yt},
-      {name: "Google UI Copy", img: ga},
+      {name: "Facebook UI Copy", img: fb, link: "https://mg-rusiana.github.io/fb_copy/"},
+      {name: "Youtube UI Copy", img: yt, link: "https://mg-rusiana.github.io/yt_copy/"},
+      {name: "Google UI Copy", img: ga, link: "https://mg-rusiana.github.io/google-account_copy/"},
     ]
 
-    const [isHovered, setIsHovered] = useState(false);
+    const [isHovered, setIsHovered] = useState(Array(projects.length).fill(false));
 
     return (
       <Element name="Projects" className=' w-full min-h-screen bg-bg flex flex-col items-center '
@@ -29,28 +29,33 @@ export const Projects = () => {
         <div className='w-[80%] text-white flex flex-col mt-[120px]'>
             <h1 className='text-t1 text-[50px] text-center'>Projects</h1>
             <div className=' w-full min-h-[300px] flex flex-wrap gap-10 justify-center items-center mt-[35px]'>
-              {projects.map((e:any)=>(
-                <div className='flex flex-col justify-center items-center p-5 rounded-md bg-[#575d5d] shadow-md gap-5'>
-                  <div className='w-[450px] h-[260px] relative hoverEffect' 
-                    style={{    
-                      backgroundImage: `url(${e.img})`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center',
-                    }}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                  >
-                    <button 
-                      className={
-                        isHovered
-                          ? 'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-t1'
-                          : 'hidden'
-                        }>Click me</button>
-                  </div>
+              {projects.map((e:any, index:number)=>(
+                  <div className='flex flex-col justify-center items-center p-5 rounded-md bg-[#575d5d] shadow-md gap-5'>
+                    <div className='w-[450px] h-[260px] relative ' 
+                      onMouseEnter={() => {
+                        const newIsHovered = [...isHovered];
+                        newIsHovered[index] = true;
+                        setIsHovered(newIsHovered);
+                      }}
+                      onMouseLeave={() => {
+                        const newIsHovered = [...isHovered];
+                        newIsHovered[index] = false;
+                        setIsHovered(newIsHovered);
+                      }}
+                    >
+                      <img src={e.img} className='w-full h-full hoverEffect z-10' style={{backgroundSize: 'cover', backgroundPosition: 'center',}} />
+                      <a href={e.link} target="_blank" rel="noopener noreferrer"><button 
+                        className={
+                          isHovered[index]
+                            ? 'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-bg text-white w-[140px] h-[40px] rounded-md'
+                            : 'hidden'
+                          }>Go to Website</button>
+                        </a>
+                    </div>
 
-                  <p className='text-[23px]'>{e.name}</p>
-                </div>
-              ))}
+                    <p className='text-[23px]'>{e.name}</p>
+                  </div>
+                ))}
             </div>
         </div>
       </Element>
