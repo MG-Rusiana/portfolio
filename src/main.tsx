4e8f86c11041533ed726a7cc330f12a52a7396a2
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-05-02 08:55:42 +0800

    Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+// import App from './App.tsx'
 import './index.css'
+import { Suspense, lazy } from "react";
+
+
+const Home=lazy(() =>
+  wait(1300).then(() => import("./App.tsx"))
+);
+
+const router = createBrowserRouter([
+
+  {
+    path: "/portfolio",
+    element: <Navigate to="/portfolio/home" />,
+  },
+  
+  {
+    path: "/portfolio/home",
+    element:  <>
+    <Suspense >
+      <Home />
+    </Suspense>
+  </>,
+  },
+  
+
+]);
+
+function wait( time:number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
