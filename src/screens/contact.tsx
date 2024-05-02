975c4921eef22ee7a6bc460d7255b030814e702e
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2024-05-02 14:39:11 +0800

    another change

--- src/screens/contact.tsx
+++ src/screens/contact.tsx
@@ -1,15 +1,41 @@
 import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedin, FaPaperPlane, FaPhone} from 'react-icons/fa';
 import { Element } from 'react-scroll';
 import { Footer } from '../components/footer';
+import { useState } from 'react';
+import { FaCircleArrowRight } from 'react-icons/fa6';
 
 export const Contact = () => {
+
+    const [isLoading, setIsLoading] = useState(false)
     return (
       <Element name="Contact" className=' w-full min-h-screen bg-bg flex flex-col items-center'>
         <div className='w-[80%] h-[100px] text-white flex flex-col mt-[120px]'>
           <h1 className='text-t1 text-[50px] md:text-center'>Contact Me</h1>
         </div>
 
-        <form method='POST' className='flex flex-col md:flex-row justify-center items-center w-[80%] text-white mb-20'>
+        <form method='POST' action='https://getform.io/f/warkpvkb' className='flex flex-col md:flex-row justify-center items-center w-[80%] text-white mb-20'
+          onSubmit={async (event:any) => {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            setIsLoading(true)
+            try {
+              const response = await fetch(event.target.action, {
+                method: 'POST',
+                body: formData,
+              });
+              if (response.ok) {
+                console.log('Form submitted successfully');
+                setIsLoading(false)
+                // You can add code here to handle successful form submission
+              } else {
+                console.error('Form submission failed');
+                // You can add code here to handle errors
+              }
+            } catch (error) {
+              console.error('Error submitting form:', error);
+            }
+          }}
+          >
           <div className=' w-[500px] md:w-[400px] flex flex-col justify-center md:items-center  p-5 '>
             <h1 className=' text-[40px] mb-5 w-[300px]'>Get in touch</h1>
             <h2 className=' text-[28px] w-[300px]'>Need something?</h2>
@@ -31,8 +57,11 @@ export const Contact = () => {
             <input className=' w-full border-[1px] rounded-md bg-bg  outline-none h-[60px] pl-3' type='text' placeholder='Email' name='email'/>
             <textarea className=' w-full border-[1px] rounded-md bg-bg  outline-none pl-3 text-start pt-3' rows={10} placeholder='Message' name='message' />
             <div className='flex flex-row justify-center items-center gap-5 rounded-md w-[200px] h-[40px] bg-t1 text-bg hover:bg-opacity-80'>
-              <button className=' outline-none '>Submit</button>
-              <FaPaperPlane />
+              <button className=' outline-none ' >Submit</button>
+              {isLoading 
+                ?<FaCircleArrowRight />
+                : <FaPaperPlane />
+              }
             </div>
             
           </div>
